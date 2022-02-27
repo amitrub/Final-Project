de12adda7013f6eabf703b95fd360571c768b585
Amit <amitrub.github.com>
2022-02-27 04:08:35 +0200

    add address to user

--- Server/addresses/models.py
+++ Server/addresses/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+# -------------------Address-------------------
+
+class Address(models.Model):
+    """Profile status update"""
+
+    country = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    street = models.CharField(max_length=255)
+    number = models.PositiveIntegerField()
+
+    user = models.OneToOneField(User,
+                            on_delete=models.CASCADE,
+                            related_name='address',
+                            null=True,
+                            blank=True)
+
+    def __str__(self):
+        """Return the model as a string"""
+        return "{} {}, {}, {}".format(self.street, str(self.number), self.city, self.country)
--- Server/addresses/urls.py
+++ Server/addresses/urls.py
@@ -1,3 +1,12 @@
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+
+from addresses import views
+
+router = DefaultRouter()
+router.register('addresses', views.AddressViewSet)
 
 urlpatterns = [
+    path('', include(router.urls))
 ]
