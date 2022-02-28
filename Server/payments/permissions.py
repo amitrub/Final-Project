7cdaec765de7bf43f373dd44280fe845f7e91480
Reut Levi <reut_l@optimove.com>
2022-02-28 19:43:37 +0200

    Add payment permission

--- Server/payments/permissions.py
+++ Server/payments/permissions.py
@@ -1,20 +1,14 @@
+from django.db.models import Q
 from rest_framework import permissions
 
-from events.models import Event
-from users.models import EventManager, User
+from payments.models import Payment
 
 
-class UpdatePayment(permissions.BasePermission):
-    """Allow users to update their own status"""
+class UpdatePayments(permissions.BasePermission):
+    """Allow user to edit their own profile"""
 
     def has_permission(self, request, view):
         """
         Return `True` if permission is granted, `False` otherwise.
         """
-        id = request.user.id
-        return User.objects.filter(pk=id).exists()
-
-    def has_object_permission(self, request, view, obj):
-        """Check the user is trying to update their own status"""
-
-        return obj.user.pk == request.user.id
+        return True
--- Server/payments/views.py
+++ Server/payments/views.py
@@ -1,9 +1,12 @@
+from django.db.models import Q
 from rest_framework import status, viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.exceptions import NotFound
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from payments import models, serializers, permissions
+from payments.models import Payment
 from users.models import User
 
 
@@ -13,8 +16,19 @@ class PaymentViewSet(viewsets.ModelViewSet):
     queryset = models.Payment.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (
-        permissions.UpdatePayment,
+        permissions.UpdatePayments,
         IsAuthenticated,
     )
 
+    def get_queryset(self, *args, **kwargs):
+        user_id = self.request.user.id
+        return self.queryset.filter(Q(to_user=user_id) | Q(from_user=user_id))
 
+    def create(self, request, *args, **kwargs):
+        from_user = request.data['from_user']
+        if str(request.user.id) != str(from_user):
+            return Response(status=status.HTTP_403_FORBIDDEN)
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
