c5015b35f7d2942750ea82a62e85347cc83f9581
Reut Levi <reut_l@optimove.com>
2022-03-03 10:17:56 +0200

    Add product app

---
+++ Server/products/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+from users.models import Supplier
+
+
+class Product(models.Model):
+    """Profile status update"""
+    supplier = models.ForeignKey(
+        Supplier,
+        on_delete=models.CASCADE,
+        related_name='from_user',
+    )
+    description = models.CharField(max_length=255)
+
+    def __str__(self):
+        """Return the model as a string"""
+        return self.description
