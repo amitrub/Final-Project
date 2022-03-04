31ef33488cde635f78df675308c13aa16d29c9b6
RoeeB <roee_b@optimove.com>
2022-03-04 17:28:09 +0200

    register + login - api flow, need to fix responses

--- Client/event-manager-client/screens/RegisterPage.js
+++ Client/event-manager-client/screens/RegisterPage.js
@@ -6,124 +6,29 @@ import {
   Button,
   TextInput,
   SafeAreaView,
+  ScrollView,
 } from "react-native";
 import Colors from "../constants/colors";
 import { useDispatch, useSelector } from "react-redux";
 import { toggleFavorite } from "../store/actions/meals";
 import * as userActions from "../store/actions/users";
+import RegisterInput from "../components/basicComponents/RegisterPage/RegisterInput";
+import LoginInput from "../components/basicComponents/RegisterPage/LoginInput";
 
 const RegisterPage = (props) => {
-  const [emailRegister, setEmailRegister] = React.useState("");
-  const [passwordRegister, setPasswordRegister] = React.useState("");
-  const [nameRegister, setNameRegister] = React.useState("");
-  const [phoneRegister, setPhoneRegister] = React.useState("");
-
-  const [emailLogin, setEmailLogin] = React.useState("");
-  const [passwordLogin, setPasswordLogin] = React.useState("");
-
-  const registeredUsers = useSelector((state) => state["users"].registered);
-
-  const dispatch = useDispatch();
-  const onPressRegister = useCallback(async () => {
-    debugger;
-    console.log("enter to onPressRegister");
-
-    await dispatch(
-      await userActions.registerApi(
-        nameRegister,
-        emailRegister,
-        passwordRegister,
-        phoneRegister
-      )
-    );
-    console.log("back to onPressRegister");
-    //Cant jump with stack navigator after dispatch
-    //props.navigation.navigate("HomePage");
-  }, [dispatch, nameRegister, emailRegister, passwordRegister, phoneRegister]);
-
-  const onPressLogin = () => {
-    console.log("onPressLogin");
-    props.navigation.navigate("HomePage");
-  };
-
   return (
-    <View style={styles.screen}>
-      {/**** Extract RegisterInput Component ****/}
-      <View style={{ paddingTop: "15%" }}>
-        <Text style={styles.mainTitle}>REGISTER</Text>
-      </View>
+    <SafeAreaView style={styles.screen}>
+      <ScrollView>
+        <RegisterInput />
 
-      <Text>{JSON.stringify(registeredUsers)}</Text>
+        <View style={{ paddingTop: "15%", alignItems: "center" }}>
+          <Text>Already registered? Sign in...</Text>
+          <Text>-------------------------------------</Text>
+        </View>
 
-      <SafeAreaView>
-        <TextInput
-          style={styles.input}
-          onChangeText={setEmailRegister}
-          value={emailRegister}
-          placeholder={"Email address"}
-        />
-        <TextInput
-          style={styles.input}
-          onChangeText={setNameRegister}
-          value={nameRegister}
-          placeholder={"Full Name"}
-        />
-        <TextInput
-          style={styles.input}
-          onChangeText={setPasswordRegister}
-          value={passwordRegister}
-          placeholder="password"
-          // keyboardType="numeric"
-        />
-        <TextInput
-          style={styles.input}
-          onChangeText={setPhoneRegister}
-          value={phoneRegister}
-          placeholder={"Phone Number"}
-        />
-      </SafeAreaView>
-
-      <Button
-        onPress={onPressRegister}
-        title="Register"
-        color="#841584"
-        accessibilityLabel="Learn more about this purple button"
-      />
-      {/**** end RegisterInput Component ****/}
-
-      {/**** Extract LoginInput Component ****/}
-      <View style={{ paddingTop: "15%" }}>
-        <Text>Have you register before?</Text>
-      </View>
-
-      <View style={{ paddingTop: "5%" }}>
-        <Text style={styles.mainTitle}>Login</Text>
-      </View>
-
-      <SafeAreaView>
-        <TextInput
-          style={styles.input}
-          onChangeText={setEmailLogin}
-          value={emailLogin}
-          placeholder={"Email address"}
-        />
-        <TextInput
-          style={styles.input}
-          onChangeText={setPasswordLogin}
-          value={passwordLogin}
-          placeholder="password"
-          // keyboardType="numeric"
-        />
-      </SafeAreaView>
-
-      <Button
-        onPress={onPressLogin}
-        title="Login"
-        color="#841584"
-        accessibilityLabel="Learn more about this purple button"
-      />
-      {/**** end LoginInput Component ****/}
-    </View>
+        <LoginInput />
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
@@ -131,22 +36,6 @@ const styles = StyleSheet.create({
   screen: {
     justifyContent: "center",
     alignItems: "center",
-  },
-  mainTitle: {
-    color: Colors.text_black,
-    fontFamily: "alef-bold",
-    fontSize: 18,
-    fontStyle: "normal",
-    fontWeight: "700",
-    lineHeight: 25,
-    textAlign: "center",
-  },
-  input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
-    width: 250,
   },
 });
 
--- Client/event-manager-client/store/actions/users.js
+++ Client/event-manager-client/store/actions/users.js
@@ -1,18 +1,27 @@
 import RegisterUser from "../../Entities/Users/RegisterUser";
 import Address from "../../Entities/Users/Address";
+import SignedInUser from "../../Entities/Users/SignedInUser";
 
-export const firebase_url =
-  "https://test-server-event-manager-default-rtdb.firebaseio.com/tests.json";
-export const local_url = "http://127.0.0.1:8000/api/user/";
-export const local_specific_url = "http://192.168.5.101:8000/api/user/";
-
+export const firebase_base_url =
+  "https://test-server-event-manager-default-rtdb.firebaseio.com";
+export const local_base_url = "http://127.0.0.1:8000";
+export const local_specific_base_url = "http://192.168.5.101:8000";
+//
+export const register = "/api/user.json";
+export const login = "/api/login.json";
+//
 export const REGISTER_USER = "REGISTER_USER";
+export const LOGIN_USER = "LOGIN_USER";
 
 export const registerApi = (
   nameRegister,
   emailRegister,
   passwordRegister,
-  phoneRegister
+  phoneRegister,
+  country,
+  city,
+  street,
+  number
 ) => {
   return async (dispatch) => {
     const user = new RegisterUser(
@@ -20,9 +29,9 @@ export const registerApi = (
       emailRegister,
       passwordRegister,
       phoneRegister,
-      new Address("country", "city", "street", "number")
+      new Address(country, city, street, number)
     );
-    await fetch(firebase_url, {
+    await fetch(firebase_base_url + register, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -32,11 +41,35 @@ export const registerApi = (
       }),
     })
       .then((response) => {
-        console.log("ActionUsers >> registerApi >> after post request");
         const responseData = response.json();
-        console.log("ActionUsers >> registerApi >> responseData:");
-        console.log(responseData);
+        //OUTPUT: nothing => need to add check OK response API_ERROR_HANDLER?
         dispatch({ type: REGISTER_USER, registerUser: user });
+      })
+      .catch((error) => {
+        console.log("Reducer Users >> register api >> error:");
+        console.log(error);
+      });
+  };
+};
+
+export const loginApi = (email, password) => {
+  return async (dispatch) => {
+    await fetch(firebase_base_url + login, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    })
+      .then((response) => {
+        const responseData = response.json();
+        //OUTPUT: token / maybe name ? to show on home page
+        // const sign_in_user = new SignedInUser(email, password, "", responseData.token);
+        const sign_in_user = new SignedInUser(email, password, "", "token1234");
+        dispatch({ type: LOGIN_USER, sign_in_user: sign_in_user });
       })
       .catch((error) => {
         console.log("Reducer Users >> register api >> error:");
