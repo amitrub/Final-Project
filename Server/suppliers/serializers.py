319fafc95b3bba9b6df0a4d9ba81133bbe36990f
Reut Levi <reut_l@optimove.com>
2022-03-24 21:51:44 +0200

    change supplier details

--- Server/suppliers/serializers.py
+++ Server/suppliers/serializers.py
@@ -1,14 +1,19 @@
 from rest_framework import serializers
 
-
 from suppliers import models
 
-
 # -------------------Suuplier-------------------
+from suppliers.models import Supplier
+
 
 class SupplierSerializer(serializers.ModelSerializer):
     """Serializer profile feed items"""
 
+    event = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='event_name'
+    )
+
     class Meta:
         model = models.Supplier
-        fields = ('id', 'name', 'price', 'advance_pay', 'pay_method','event')
+        fields = ('id', 'name', 'price', 'advance_pay', 'pay_method', 'event')
