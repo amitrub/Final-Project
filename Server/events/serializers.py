7a03c3836974e98d0137ce90e217416da22493a3
Reut Levi <reut_l@optimove.com>
2022-03-26 17:41:15 +0300

    add suppliers to comment

--- Server/events/serializers.py
+++ Server/events/serializers.py
@@ -5,22 +5,38 @@ from event_owner.serializers import EventOwnerSerializer
 from events import models
 
 # -------------------Event-------------------
-from events.models import Event
+from events.models import Event, EventSchedule
 from meetings.models import Meetings
 from meetings.serializers import MeetingSerializer
 from suppliers.models import Supplier
 from suppliers.serializers import SupplierSerializer
 
+# -------------------EventSchedule-------------------
+
+class EventScheduleSerializer(serializers.ModelSerializer):
+    """Serializer profile feed items"""
+
+    event = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='event_name'
+    )
+
+    class Meta:
+        model = models.EventSchedule
+        fields = ('id', 'event', 'start_time', 'end_time', 'description')
+        # fields = '__all__'
 
 class EventSerializer(serializers.ModelSerializer):
     """Serializer profile feed items"""
     # suppliers = SupplierSerializer(many=True, required=False)
     # meetings = MeetingSerializer(many=True, required=False)
     event_owners = EventOwnerSerializer(many=True, required=False)
+    suppliers = SupplierSerializer(many=True, required=False)
+    event_schedules = EventScheduleSerializer(many=True, required=False)
 
     class Meta:
         model = models.Event
-        fields = ('id', 'event_manager', 'type', 'event_name', 'date', 'budget', 'location', 'event_owners')
+        fields = ('id', 'event_manager', 'type', 'event_name', 'date', 'budget', 'location', 'event_owners','suppliers','event_schedules')
         # fields = '__all__'
         extra_kwargs = {
             'event_manager': {
@@ -38,33 +54,24 @@ class EventSerializer(serializers.ModelSerializer):
             budget=validated_data['budget'],
             location=validated_data['location'],
         )
+        print(validated_data)
         if 'event_owners' in validated_data:
             event_owners = validated_data.pop('event_owners')
-            print(event_owners)
             for event_owner in event_owners:
                 event_owner_new = EventOwner.objects.create(**event_owner, event_id=event.id)
-        # if 'meetings' in validated_data:
-        #     meetings = validated_data.pop('meetings')
-        #     for meeting in meetings:
-        #         meeting_new = Meetings.objects.create(event_manager=validated_data['event_manager'], **meeting)
-        #         meeting_new.event.add(event)
+        if 'suppliers' in validated_data:
+            suppliers = validated_data.pop('suppliers')
+            for supplier in suppliers:
+                Supplier.objects.create(event_id=event.id, **supplier)
+        if 'event_schedules' in validated_data:
+            event_sch = validated_data.pop('event_schedules')
+            for schedule in event_sch:
+                EventSchedule.objects.create(event_id=event.id, **schedule)
+        print(event)
         return event
 
 
-# -------------------EventSchedule-------------------
 
-class EventScheduleSerializer(serializers.ModelSerializer):
-    """Serializer profile feed items"""
-
-    event = serializers.SlugRelatedField(
-        read_only=True,
-        slug_field='event_name'
-    )
-
-    class Meta:
-        model = models.EventSchedule
-        fields = ('id', 'event', 'start_time', 'end_time', 'description')
-        # fields = '__all__'
 
 # class MeetingSerializer(serializers.ModelSerializer):
 #     """Serializer profile feed items"""
