ed15d86eb88ad82c41a06142c7273e78e84de5f3
RoeeB <roee_b@optimove.com>
2022-04-28 12:16:58 +0300

    reset main

--- Client/event-manager-client/screens/main-screens/WelcomePage.js
+++ Client/event-manager-client/screens/main-screens/WelcomePage.js
@@ -4,44 +4,14 @@ import TitleButton from "../../components/basicComponents/buttons/TitleButton";
 import LogoImage from "../../components/basicComponents/WelcomePage/LogoImage";
 import Log from "../../constants/logger";
 import LoginInput from "../../components/basicComponents/RegisterPage/LoginInput";
-import {WelcomePageStyles} from "../../Styles/styles";
-import * as Google from 'expo-google-app-auth';
+import { WelcomePageStyles } from "../../Styles/styles";
 
 const WelcomePage = (props) => {
   Log.info("Welcome Page >> loading");
   const [isLogin, setIsLogin] = useState(false);
-  const [isLoginGoogle, setIsLoginGoogle] = useState(false)
 
   const onPressRegister = () => {
     props.navigation.navigate("Register");
-  };
-
-  const SignIngoogle = () => {
-    const config = {
-      iosClientId: '281217241179-9vvln5etsdo6k1tq26ajbka5tsqhucr9.apps.googleusercontent.com',
-      androidClientId: '281217241179-0l1u9546ujv8qkkd7khflc262cutcl2a.apps.googleusercontent.com',
-      scopes:['profile','email']
-    };
-
-    Google
-        .logInAsync(config)
-        .then((result)=>
-        {
-          console.log(result)
-          const{type,user} = result;
-          if(type==='success'){
-            setIsLoginGoogle(!isLoginGoogle);
-            const {email,name,photourl} = user;
-            setTimeout(() => props.navigation.navigate("Welcome"), {email,name, photourl},1000)
-            props.navigation.navigate("Welcome");
-          }
-          else {
-            console.log("error");
-          }
-        })
-        .catch(error => {
-          console.log(error);
-        })
   };
 
   const onPressLogin = () => {
@@ -64,18 +34,10 @@ const WelcomePage = (props) => {
             <View style={{ paddingTop: "15%" }}>
               <TitleButton text={"Login"} onPress={onPressLogin} />
               <TitleButton text={"Register"} onPress={onPressRegister} />
-              <TitleButton
-                text={"Sign-in with Google"}
-                onPress={SignIngoogle}
-              />
-              <TitleButton
-                  text={"Reut"}
-                  onPress={SignIngoogle}
-              />
             </View>
           </View>
         ) : (
-          <LoginInput onLogin={SignIngoogle} />
+          <LoginInput onLogin={onPressLogin} />
         )}
       </View>
     </ScrollView>
