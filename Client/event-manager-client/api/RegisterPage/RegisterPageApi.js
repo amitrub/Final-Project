0086d551b1d941224395906fc97f072fb14f13dc
hadasze <hadasz@wix.com>
2022-06-11 13:21:42 +0300

    fixing rubin ch

--- Client/event-manager-client/api/RegisterPage/RegisterPageApi.js
+++ Client/event-manager-client/api/RegisterPage/RegisterPageApi.js
@@ -1,45 +1,38 @@
-import { base_url, register } from "../../constants/urls";
-import {
-  createOneButtonAlert,
-  STATUS_FAILED,
-  STATUS_SUCCESS,
-} from "../../constants/errorHandler";
-import Log, { logApiRequest } from "../../constants/logger";
-import { logAndCreateErrorMessage } from "../../validations/validations";
+import {base_url, register} from "../../constants/urls";
+import {createOneButtonAlert, STATUS_FAILED, STATUS_SUCCESS,} from "../../constants/errorHandler";
+import {logApiRequest} from "../../constants/logger";
+import {logAndCreateErrorMessage} from "../../validations/validations";
 import {global_timeout, global_timeout_message} from "../../global/GlobalValues";
 
 export async function registerUserRequest(
-  user,
-  emptyRegisterInputs,
-  navigation,
-  myContext
+    user,
+    emptyRegisterInputs,
+    navigation,
 ) {
-  const { setIsLoading } = myContext;
-  let functionName = "Register User Request";
-  let url = base_url + register;
-  let request = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(user),
-  };
-  logApiRequest(functionName, url, request);
-  await fetch(url, request, { timeout: global_timeout })
-    .then(async (res) => {
-      const data = await res.json();
-      if (STATUS_FAILED(res.status)) {
-        logAndCreateErrorMessage(data, functionName);
-      } else if (STATUS_SUCCESS(res.status)) {
-        const message = "You have successfully registered!\nplease LOGIN";
-        createOneButtonAlert(message, "OK", "Registration Succeeded", () =>
-          navigation.navigate("Welcome")
-        );
-        emptyRegisterInputs();
-      }
-    })
-    .catch((error) => {
-      setIsLoading(false);
-      logAndCreateErrorMessage({"Error": global_timeout_message}, functionName);
-    });
+    let functionName = "Register User Request";
+    let url = base_url + register;
+    let request = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+    };
+    logApiRequest(functionName, url, request);
+    await fetch(url, request, {timeout: global_timeout})
+        .then(async (res) => {
+            const data = await res.json();
+            if (STATUS_FAILED(res.status)) {
+                logAndCreateErrorMessage(data, functionName);
+            } else if (STATUS_SUCCESS(res.status)) {
+                const message = "You have successfully registered!\nplease LOGIN";
+                createOneButtonAlert(message, "OK", "Registration Succeeded", () =>
+                    navigation.navigate("Welcome")
+                );
+                emptyRegisterInputs();
+            }
+        })
+        .catch((error) => {
+            logAndCreateErrorMessage({"Error": global_timeout_message}, functionName);
+        });
 }
